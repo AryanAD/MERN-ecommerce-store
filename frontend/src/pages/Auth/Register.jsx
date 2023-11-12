bcd39f8387145c504c7ab9b5413d1e8b3c02151d
AryanAD <aryanhohai@gmail.com>
2023-11-12 16:56:24 +0545

    Register completed

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -1,5 +1,146 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation, useNavigate } from "react-router";
+import { useRegisterMutation } from "../../redux/api/usersApiSlice";
+import Loader from "../../components/Loader";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import { setCredentials } from "../../redux/features/auth/authSlice";
+
 const Register = () => {
-  return <></>;
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [register, { isLoading }] = useRegisterMutation();
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const { search } = useLocation();
+  const sp = new URLSearchParams(search);
+  const redirect = sp.get("redirect") || "/";
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [navigate, redirect, userInfo]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match");
+    }
+    try {
+      const res = await register({
+        userName,
+        email,
+        password,
+        confirmPassword,
+      }).unwrap();
+      console.log(res);
+      dispatch(setCredentials({ ...res }));
+      navigate(redirect);
+      toast.success("User registered successfully");
+    } catch (err) {
+      console.log(err);
+      toast.error(err?.data?.message || err.error);
+    }
+  };
+
+  return (
+    <section className="pl-[10rem] flex flex-wrap">
+      <div className="mr-[4rem] mt-[5rem]">
+        <h1 className="text-2xl font-semibold mb-4">Register</h1>
+
+        <form onSubmit={handleSubmit} className="container w-[40rem]">
+          <div className="my-[2rem]">
+            <label htmlFor="name" className="block text-sm font-medium">
+              Name
+            </label>
+
+            <input
+              type="text"
+              id="name"
+              className="mt-1 p-2 border rounded w-full"
+              placeholder="Enter Name"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+          </div>
+          <div className="my-[2rem]">
+            <label htmlFor="email" className="block text-sm font-medium">
+              Email
+            </label>
+
+            <input
+              type="email"
+              id="email"
+              className="mt-1 p-2 border rounded w-full"
+              placeholder="Enter Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="my-[2rem]">
+            <label htmlFor="password" className="block text-sm font-medium">
+              Password
+            </label>
+
+            <input
+              type="password"
+              id="password"
+              className="mt-1 p-2 border rounded w-full"
+              placeholder="Enter Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="my-[2rem]">
+            <label
+              htmlFor="confirmpassword"
+              className="block text-sm font-medium"
+            >
+              Confirm Password
+            </label>
+
+            <input
+              type="password"
+              id="confirmPassword"
+              className="mt-1 p-2 border rounded w-full"
+              placeholder="Confirm Password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </div>
+          <button
+            disabled={isLoading}
+            type="submit"
+            className="bg-[#b24747] font-medium px-4 py-2 rounded cursor-pointer my-[1rem]"
+          >
+            {isLoading ? "Registering..." : "Registered"}
+          </button>
+
+          {isLoading && <Loader />}
+        </form>
+
+        <div className="mt-4">
+          <p>
+            Already have an account?{" "}
+            <Link
+              to={redirect ? `/login?redirect=${redirect}` : "/login"}
+              className="text-[#903042] font-medium hover:underline"
+            >
+              Login
+            </Link>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Register;
