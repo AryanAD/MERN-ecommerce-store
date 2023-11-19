2b2b0aa3f2dc6297c3f2da08ea3f5e3a6e646667
AryanAD <aryanhohai@gmail.com>
2023-11-19 20:23:41 +0545

    adminRoutes

--- frontend/src/pages/Admin/AdminRoute.jsx
+++ frontend/src/pages/Admin/AdminRoute.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router";
 
 const AdminRoute = () => {
-  return <div></div>;
+  const { userInfo } = useSelector((state) => state.auth);
+
+  return userInfo && userInfo.isAdmin ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" replace />
+  );
 };
 
 export default AdminRoute;
