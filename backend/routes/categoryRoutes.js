2e5c379a41fd0a37d5de66e42f6f1cdd2cbe1b57
AryanAD <aryanhohai@gmail.com>
2023-11-28 15:35:51 +0545

    backend

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 const router = express.Router();
+import createCategory from "../controllers/categoryController.js";
 
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
