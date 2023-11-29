b5d70e3f82c0a97b95b0e35ff8d5cb3c683fc66a
AryanAD <aryanhohai@gmail.com>
2023-11-29 12:30:58 +0545

    updateCategory

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -22,4 +22,26 @@ const createCategory = asyncHandler(async (req, res) => {
   }
 });
 
-export default createCategory;
+const updateCategory = asyncHandler(async (req, res) => {
+  try {
+    const { name } = req.body;
+    const { categoryId } = req.params;
+
+    const category = await Category.findOne({ _id: categoryId });
+
+    if (!category) {
+      return res.status(404).json({ error: "Category not found" });
+    }
+
+    category.name = name;
+
+    const updatedCategory = await category.save();
+    res.json(updatedCategory);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+export { createCategory, updateCategory };
