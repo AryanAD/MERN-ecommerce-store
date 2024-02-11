e5d5d4ab584f417c27992c3974bae5f4fba89262
AryanAD <aryanhohai@gmail.com>
2024-02-11 19:25:04 +0545

    update Product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,11 +3,17 @@ import formiddable from "express-formidable";
 const router = express.Router();
 
 // controllers
-import { addProduct } from "../controllers/productController.js";
+import {
+  addProduct,
+  updateProductDetails,
+} from "../controllers/productController.js";
 
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 import checkId from "../middlewares/checkId.js";
 
 router.route("/").post(authenticate, authorizeAdmin, formiddable(), addProduct);
+router
+  .route("/:id")
+  .put(authenticate, authorizeAdmin, formiddable(), updateProductDetails);
 
 export default router;
