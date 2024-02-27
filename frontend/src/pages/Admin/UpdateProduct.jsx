b1745776e9206dbcc6cb988908b4b036b2dea088
AryanAD <aryanhohai@gmail.com>
2024-02-27 08:35:39 +0545

    updateProduct imports and data handling

--- frontend/src/pages/Admin/UpdateProduct.jsx
+++ frontend/src/pages/Admin/UpdateProduct.jsx
@@ -1,6 +1,50 @@
-import React from "react";
+import { useNavigate, useParams } from "react-router";
+import {
+  useDeleteProductMutation,
+  useGetProductByIdQuery,
+  useUpdateProductMutation,
+  useUploadProductImageMutation,
+} from "../../redux/api/productApiSlice";
+import { useEffect, useState } from "react";
+import { useFetchCategoriesQuery } from "../../redux/api/categoryApiSlice";
 
 const UpdateProduct = () => {
+  const params = useParams();
+  const navigate = useNavigate();
+
+  const { data: productData } = useGetProductByIdQuery(params?._id);
+  const { data: categories = [] } = useFetchCategoriesQuery();
+
+  const [uploadProductImage] = useUploadProductImageMutation();
+  const [updateProduct] = useUpdateProductMutation();
+  const [deleteProduct] = useDeleteProductMutation();
+
+  const [name, setName] = useState(productData?.name || "");
+  const [image, setImage] = useState(productData?.image || "");
+  const [price, setPrice] = useState(productData?.price || "");
+  const [brand, setBrand] = useState(productData?.brand || "");
+  const [quantity, setQuantity] = useState(productData?.quantity || "");
+  const [category, setCategory] = useState(productData?.category || "");
+  const [description, setDescription] = useState(
+    productData?.description || ""
+  );
+  const [stockQuantity, setStockQuantity] = useState(
+    productData?.countInStock || ""
+  );
+
+  useEffect(() => {
+    if (productData && productData?._id) {
+      setName(productData?.name);
+      setImage(productData?.image);
+      setPrice(productData?.price);
+      setBrand(productData?.brand);
+      setQuantity(productData?.quantity);
+      setCategory(productData?.categories?._id);
+      setDescription(productData?.description);
+      //   setStockQuantity(productData?.countInStock);
+    }
+  }, [productData]);
+
   return <div>UpdateProduct</div>;
 };
 
