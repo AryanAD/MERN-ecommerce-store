02a80a1d48b41ddf8bd5c2afb44a163dd0b19d23
AryanAD <aryanhohai@gmail.com>
2024-04-21 21:31:16 +0545

    created few functions in orderControllers

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,12 +1,99 @@
 import Order from "../models/orderModel.js";
 import Product from "../models/productModel.js";
 
+// Utility Function
+function calcPrices(orderItems) {
+  const itemsPrice = orderItems.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
+
+  const shippingPrice = itemsPrice > 100 ? 0 : 10;
+  const taxRate = 0.15;
+  const taxPrice = (itemsPrice * taxRate).toFixed(2);
+
+  const totalPrice = (
+    itemsPrice +
+    shippingPrice +
+    ParseFloat(taxPrice)
+  ).toFixed(2);
+
+  return {
+    itemsPrice: itemsPrice.toFixed(2),
+    shippingPrice: shippingPrice.toFixed(2),
+    taxPrice,
+    totalPrice,
+  };
+}
+
 const createOrder = async (req, res) => {
   try {
-    res.send("HELLO WORLD THIS IS CREATE ORDER");
+    const { orderItems, shippingAddress, paymentMethod } = req.body;
+
+    if (orderItems && orderItems.length == 0) {
+      res.status(400);
+      throw new Error("Please add at least one product to your order");
+    }
+
+    const itemsFromDB = await Product.find({
+      _id: { $in: orderItems.map((order) => order.productId) },
+    });
+
+    const dbOrderItems = orderItems.map((itemFromClient) => {
+      const matchingItemFromDB = itemsFromDB.find(
+        (itemFromDB) => itemFromDB._id.toString() === itemFromClient._id
+      );
+
+      if (!matchingItemFromDB) {
+        res.status(404);
+        throw new Error(`Product not found: ${itemFromClient._id}`);
+      }
+
+      return {
+        ...itemFromClient,
+        product: itemFromClient._id,
+        price: matchingItemFromDB.price,
+        _id: undefined,
+      };
+    });
+
+    const { itemsPrice, taxPrice, shippingPrice, totalPrice } =
+      calcPrices(dbOrderItems);
+
+    const order = new Order({
+      orderItems: dbOrderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+
+    const createdOrder = await order.save();
+    res.status(201).json(createdOrder);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
-export { createOrder };
+const getAllOrders = async (req, res) => {
+  try {
+    const orders = await Order.find({}).populate("user", "id username");
+    res.json(orders);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const getUserOrders = async (req, res) => {
+  try {
+    const orders = await Order.find({ user: req.user._id });
+    res.json(orders);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export { createOrder, getAllOrders, getUserOrders };
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,8 +2,16 @@ import express from "express";
 const router = express.Router();
 
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
-import { createOrder } from "../controllers/orderController.js";
+import {
+  createOrder,
+  getAllOrders,
+  getUserOrders,
+} from "../controllers/orderController.js";
 
-router.route("/").post(authenticate, createOrder);
+router
+  .route("/")
+  .post(authenticate, createOrder)
+  .get(authenticate, authorizeAdmin, getAllOrders);
 
+router.route("/mine").get(authenticate, getUserOrders);
 export default router;
