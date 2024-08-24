53643f7ab783a0ac659767b6451eb316b62885dc
AryanAD <aryanhohai@gmail.com>
2024-08-24 21:27:00 +0545

    finished dashboard

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -33,6 +33,7 @@ import PlaceOrder from "./pages/Orders/PlaceOrder.jsx";
 import Order from "./pages/Orders/Order.jsx";
 import UserOrder from "./pages/User/UserOrder.jsx";
 import OrderList from "./pages/Admin/OrderList.jsx";
+import AdminDashboard from "./pages/Admin/AdminDashboard.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -61,6 +62,7 @@ const router = createBrowserRouter(
         <Route path="product-update/:_id" element={<UpdateProduct />} />
         <Route path="products-list" element={<ListProducts />} />
         <Route path="orders-list" element={<OrderList />} />
+        <Route path="dashboard" element={<AdminDashboard />} />
       </Route>
     </Route>
   )
